4b8cdfc52acd0aef8858a222b182eb6140698ec0
Karan Godara <godarakaran4501@gmail.com>
2022-10-26 15:40:39 +0530

    Login interface

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 # Python program to implement client side of chat room.
+from operator import truediv
 import socket
 import select
 import sys
@@ -6,32 +7,56 @@ import sys
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 if len(sys.argv) != 3:
-	print ("Correct usage: script, IP address, port number")
-	exit()
+    print("Correct usage: script, IP address, port number")
+    exit()
 IP_address = str(sys.argv[1])
 Port = int(sys.argv[2])
 server.connect((IP_address, Port))
 
-success=False
+success = False
 while not success:
-	x=input("1. Login\n2. Sign Up\n3. Quit")
-	if x==1:
-		usr=input("Enter user name")
-		pwd=input("Enter your password")
-	elif x==2:
-		usr=input("Enter user name")
-		pwd=input("Enter your password")
-	elif x==3:
-		exit()
-	else:
-		print("Invalid input")
+    x = input("1. Login\n2. Sign Up\n3. Quit")
+    if x == 1:
+        usr = input("Enter user name")
+        pwd = input("Enter your password")
+        to_send = "{}:{}:{}".format(1, usr, pwd).encode('utf-8')
+        if (len(to_send) > 512):
+            print("Exceeded maximum length \nRetry")
+            continue
+
+        server.sendall(to_send)
+        confirm = server.recv(1)
+        confirm = confirm.decode('utf-8')
+        if (confirm == "y"):
+            success = True
+
+    elif x == 2:
+        usr = input("Enter user name")
+        pwd = input("Enter your password")
+        to_send = "{}:{}:{}".format(2, usr, pwd).encode('utf-8')
+        if (len(to_send) > 512):
+            print("Exceeded maximum length \nRetry")
+            continue
+
+        server.sendall(to_send)
+        confirm = server.recv(1)
+        confirm = confirm.decode('utf-8')
+        if (confirm == "y"):
+            success = True
+
+    elif x == 3:
+        server.sendall(b"quit")
+        exit()
+
+    else:
+        print("Invalid Input")
 
 while True:
 
-	# maintains a list of possible input streams
-	sockets_list = [sys.stdin, server]
+    # maintains a list of possible input streams
+    sockets_list = [sys.stdin, server]
 
-	""" There are two possible input situations. Either the
+    """ There are two possible input situations. Either the
 	user wants to give manual input to send to other people,
 	or the server is sending a message to be printed on the
 	screen. Select returns from sockets_list, the stream that
@@ -39,18 +64,18 @@ while True:
 	to send a message, then the if condition will hold true
 	below.If the user wants to send a message, the else
 	condition will evaluate as true"""
-	read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
+    read_sockets, write_socket, error_socket = select.select(
+        sockets_list, [], [])
 
-	for socks in read_sockets:
-		if socks == server:
-			message = socks.recv(2048)
-			print (message.decode('utf-8'))
-		else:
-			message = sys.stdin.readline()
-			server.send(bytes(message, 'utf-8'))
-			sys.stdout.write("<You>")
-			sys.stdout.write(message)
-			sys.stdout.flush()
+    for socks in read_sockets:
+        if socks == server:
+            message = socks.recv(2048)
+            print(message.decode('utf-8'))
+        else:
+            message = sys.stdin.readline()
+            server.send(bytes(message, 'utf-8'))
+            sys.stdout.write("<You>")
+            sys.stdout.write(message)
+            sys.stdout.flush()
 
 server.close()
-
--- server.py
+++ server.py
@@ -16,8 +16,8 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 # checks whether sufficient arguments have been provided
 if len(sys.argv) != 3:
-	print ("Correct usage: script, IP address, port number")
-	exit()
+    print("Correct usage: script, IP address, port number")
+    exit()
 
 # takes the first argument from command prompt as IP address
 IP_address = str(sys.argv[1])
@@ -41,8 +41,8 @@ server.listen(100)
 list_of_clients = []
 
 
-
-dbconn = psycopg2.connect(database="fastchat", user = "postgres", password = "", host = "127.0.0.1", port = "5432")
+dbconn = psycopg2.connect(database="fastchat", user="postgres",
+                          password="", host="127.0.0.1", port="5432")
 cur = dbconn.cursor()
 cur.execute('''CREATE TABLE IF NOT EXISTS GROUPS
       (NAME VARCHAR(50) NOT NULL,
@@ -56,78 +56,115 @@ cur.execute('''CREATE TABLE IF NOT EXISTS CREDENTIALS
 
 dbconn.commit()
 
+
 def clientthread(conn, addr):
 
-	# sends a message to the client whose user object is conn
-	conn.send(b"Welcome to this chatroom!")
+    # sends a message to the client whose user object is conn
+    conn.send(b"Welcome to this chatroom!")
+    success = False
+    while not success:
+        inp = conn.recv(512)
+        inp = inp.decode('utf-8')
+        if (inp == "quit"):
+            remove(conn)
+            return
 
-	while True:
-			try:
-				message = conn.recv(2048)
-				if message:
+        inp = inp.split(":")
+        to_check = f"SELECT USERNAME, PASSWORD FROM CREDENTIALS WHERE USERNAME = {inp[1]} "
+        selected_entry = cur.execute(to_check).fetchall()
 
-					"""prints the message and address of the
-					user who just sent the message on the server
-					terminal"""
-					print ("<" + addr[0] + "> " + message.decode('utf-8'))
+        if (len(selected_entry) == 0):
+            if (inp[0] == "1"):
+                conn.send(bytes("n", 'utf-8'))
+            else:
+                to_insert = "INSERT INTO CREDENTIALS VALUES (?,?,?) "
+                cur.execute(to_insert, (inp[1], inp[2], "true"))
+                conn.send(bytes("y", 'utf-8'))
+                success = True
+        else:
+            if (inp[0] == "2"):
+                conn.send(bytes("n", 'utf-8'))
+            else:
+                if (inp[2] == selected_entry[0][1]):
+                    conn.send(bytes("y", 'utf-8'))
+                    success = True
+                else:
+                    conn.send(bytes("n", 'utf-8'))
 
-					# Calls broadcast function to send message to all
-					message_to_send = "<" + addr[0] + "> " + message.decode('utf-8')
-					print('here')
-					broadcast(message_to_send, conn)
-					print('m out')
+    while True:
+        try:
+            message = conn.recv(2048)
+            if message:
 
-				else:
-					"""message may have no content if the connection
-					is broken, in this case we remove the connection"""
-					remove(conn)
+                """prints the message and address of the
+                user who just sent the message on the server
+                terminal"""
+                print("<" + addr[0] + "> " + message.decode('utf-8'))
 
-			except:
-				continue
+                # Calls broadcast function to send message to all
+                message_to_send = "<" + addr[0] + \
+                    "> " + message.decode('utf-8')
+                print('here')
+                broadcast(message_to_send, conn)
+                print('m out')
+
+            else:
+                """message may have no content if the connection
+                is broken, in this case we remove the connection"""
+                remove(conn)
+
+        except:
+            continue
+
 
 """Using the below function, we broadcast the message to all
 clients who's object is not the same as the one sending
 the message """
-def broadcast(message, connection):
-	print('entered server 123')
-	for clients in list_of_clients:
-		if clients!=connection:
-			print('entered server')
-			try:
-				clients.send(bytes(message, 'utf-8'))
-			except:
-				clients.close()
 
-				# if the link is broken, we remove the client
-				remove(clients)
+
+def broadcast(message, connection):
+    print('entered server 123')
+    for clients in list_of_clients:
+        if clients != connection:
+            print('entered server')
+            try:
+                clients.send(bytes(message, 'utf-8'))
+            except:
+                clients.close()
+
+                # if the link is broken, we remove the client
+                remove(clients)
+
 
 """The following function simply removes the object
 from the list that was created at the beginning of
 the program"""
+
+
 def remove(connection):
-	if connection in list_of_clients:
-		list_of_clients.remove(connection)
+    if connection in list_of_clients:
+        list_of_clients.remove(connection)
+
 
 while True:
 
-	"""Accepts a connection request and stores two parameters,
-	conn which is a socket object for that user, and addr
-	which contains the IP address of the client that just
-	connected"""
-	conn, addr = server.accept()
+    """Accepts a connection request and stores two parameters,
+    conn which is a socket object for that user, and addr
+    which contains the IP address of the client that just
+    connected"""
+    conn, addr = server.accept()
 
-	"""Maintains a list of clients for ease of broadcasting
+    """Maintains a list of clients for ease of broadcasting
 	a message to all available people in the chatroom"""
-	list_of_clients.append(conn)
+    list_of_clients.append(conn)
 
-	# prints the address of the user that just connected
-	print (addr[0] + " connected")
+    # prints the address of the user that just connected
+    print(addr[0] + " connected")
 
-	# creates and individual thread for every user
-	# that connects
-	start_new_thread(clientthread,(conn,addr))	
+    # creates and individual thread for every user
+    # that connects
+    start_new_thread(clientthread, (conn, addr))
 
 dbconn.close()
 conn.close()
 server.close()
-
