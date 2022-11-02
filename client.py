bd667f67821e37666c60711b6a9d73f171208b60
Karan Godara <rob.com>
2022-11-03 01:33:45 +0530

    Implemented sending of messages to both offline and online users

--- client.py
+++ client.py
@@ -1,56 +1,168 @@
 # Python program to implement client side of chat room.
+from operator import truediv
 import socket
 import select
 import sys
+import threading
+
+from _thread import *
 
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 if len(sys.argv) != 3:
-	print ("Correct usage: script, IP address, port number")
-	exit()
+    print("Correct usage: script, IP address, port number")
+    exit()
 IP_address = str(sys.argv[1])
 Port = int(sys.argv[2])
 server.connect((IP_address, Port))
 
-success=False
+
+def user_interface():
+    while (True):
+        choice1 = input(
+            "What do you want to do?\n1. For managing groups enter 'g'\n2. For sending a message in group enter 'b'\n3. For sending a DM enter 'd'\n4. To logout enter 'l'\n")
+
+        if (choice1 == 'g'):
+            server.sendall(choice1.encode('utf-8'))
+            choice2 = input(
+                "Group Settings\n1. For creating a new group enter 'n'\n2. For managing an existing group enter 'e'\n3. To return to previous menu enter 'q'\n")
+            if (choice2 == 'n'):
+                grp_name = input("Enter group name:")
+                pass
+            elif (choice2 == 'e'):
+                pass
+            elif (choice2 == 'q'):
+                continue
+            else:
+                print("Please give valid input")
+
+        elif (choice1 == 'b'):
+            server.sendall(choice1.encode('utf-8'))
+            pass
+
+        elif (choice1 == 'd'):
+            server.sendall(choice1.encode('utf-8'))
+
+            receiver = input("Who do you want to send the message to ?\n")
+            server.sendall(receiver.encode('utf-8'))
+
+            message = input("Please enter the message\n")
+            server.sendall(message.encode('utf-8'))
+
+        elif (choice1 == 'l'):
+            server.sendall(choice1.encode('utf-8'))
+            pass
+        else:
+            print("Please give valid input")
+
+
+def receiving_func():
+    # message not none
+
+    while (True):
+        message = server.recv(2048)
+        print(message.decode('utf-8'))
+
+
+success = False
 while not success:
-	x=input("1. Login\n2. Sign Up\n3. Quit")
-	if x==1:
-		usr=input("Enter user name")
-		pwd=input("Enter your password")
-	elif x==2:
-		usr=input("Enter user name")
-		pwd=input("Enter your password")
-	elif x==3:
-		exit()
-	else:
-		print("Invalid input")
+    x = int(input("1. Login\n2. Sign Up\n3. Quit\n\n"))
+    usr = ""
+    if x == 1:
+        usr += input("Enter user name: ")
+        pwd = input("Enter your password: ")
+        to_send = "{}:{}:{}".format(1, usr, pwd).encode('utf-8')
+        if (len(to_send) > 512):
+            print("Exceeded maximum length \nRetry\n")
+            continue
+
+        server.sendall(to_send)
+        confirm = server.recv(1)
+        confirm = confirm.decode('utf-8')
+        if (confirm == "y"):
+            success = True
+            print('Successfully logged in\n')
+        else:
+            print("Invalid username or password\n")
+
+    elif x == 2:
+        usr += input("Enter user name: ")
+        pwd = input("Enter your password: ")
+        to_send = "{}:{}:{}".format(2, usr, pwd).encode('utf-8')
+        if (len(to_send) > 512):
+            print("Exceeded maximum length \nRetry\n")
+            continue
+
+        server.sendall(to_send)
+        confirm = server.recv(1)
+        confirm = confirm.decode('utf-8')
+        print(confirm)
+        if (confirm == "y"):
+            success = True
+            print('Successfully signed up\n')
+        else:
+            print("Username already taken\n")
+
+    elif x == 3:
+        server.sendall(b"quit")
+        exit()
+
+    else:
+        print("Invalid Input")
+# user is now logged in
 
 while True:
 
-	# maintains a list of possible input streams
-	sockets_list = [sys.stdin, server]
+    # maintains a list of possible input streams
+    sockets_list = [sys.stdin, server]
 
-	""" There are two possible input situations. Either the
-	user wants to give manual input to send to other people,
-	or the server is sending a message to be printed on the
-	screen. Select returns from sockets_list, the stream that
-	is reader for input. So for example, if the server wants
-	to send a message, then the if condition will hold true
-	below.If the user wants to send a message, the else
-	condition will evaluate as true"""
-	read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
+    """ There are two possible input situations. Either the
+        user wants to give manual input to send to other people,
+        or the server is sending a message to be printed on the
+        screen. Select returns from sockets_list, the stream that
+        is reader for input. So for example, if the server wants
+        to send a message, then the if condition will hold true
+        below.If the user wants to send a message, the else
+        condition will evaluate as true"""
 
-	for socks in read_sockets:
-		if socks == server:
-			message = socks.recv(2048)
-			print (message.decode('utf-8'))
-		else:
-			message = sys.stdin.readline()
-			server.send(bytes(message, 'utf-8'))
-			sys.stdout.write("<You>")
-			sys.stdout.write(message)
-			sys.stdout.flush()
+    # start_new_thread(receiving_func, ())
+    # start_new_thread(user_interface, ())
+
+    # read_sockets, write_socket, error_socket = select.select(
+    # sockets_list, [], [])
+    # print("tttttttttt")
+    # print(read_sockets)
+    # print("tttttttttt")
+
+    thread1 = threading.Thread(target=receiving_func, args=())
+    thread2 = threading.Thread(target=user_interface, args=())
+    # Starting thread 1
+    thread1.start()
+
+   # Starting thread 2
+    thread2.start()
+
+    # Wait until thread 1 is completely executed
+    thread1.join()
+
+   # Wait until thread 2 is completely executed
+    thread2.join()
+
+    # for socks in read_sockets:
+    #     if socks == server:
+    #         # print("ttt")
+    #         start_new_thread(receiving_func, ()).start()
+    #     else:
+    #         inp = sys.stdin.readline().strip()
+    #         start_new_thread(user_interface, ()).start()
+    # print("aa")
+    # start_new_thread(receiving_func, ()).join()
+    # start_new_thread(user_interface, ()).join()
+    # message = sys.stdin.readline()
+    # server.send(bytes(message, 'utf-8'))
+    # sys.stdout.write("<You>")
+    # sys.stdout.write(message)
+    # sys.stdout.flush()
+
 
 server.close()
-
--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import socket
 import select
 import sys
 import psycopg2
+
 '''Replace "thread" with "_thread" for python 3'''
 from _thread import *
 
@@ -16,8 +17,8 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 # checks whether sufficient arguments have been provided
 if len(sys.argv) != 3:
-	print ("Correct usage: script, IP address, port number")
-	exit()
+    print("Correct usage: script, IP address, port number")
+    exit()
 
 # takes the first argument from command prompt as IP address
 IP_address = str(sys.argv[1])
@@ -39,10 +40,10 @@ increased as per convenience.
 server.listen(100)
 
 list_of_clients = []
+username_conn = {}
 
-
-
-dbconn = psycopg2.connect(database="fastchat", user = "postgres", password = "", host = "127.0.0.1", port = "5432")
+dbconn = psycopg2.connect(database="fastchat", user="postgres",
+                          password="", host="127.0.0.1", port="5432")
 cur = dbconn.cursor()
 cur.execute('''CREATE TABLE IF NOT EXISTS GROUPS
       (NAME VARCHAR(50) NOT NULL,
@@ -51,83 +52,162 @@ cur.execute('''CREATE TABLE IF NOT EXISTS GROUPS
 
 cur.execute('''CREATE TABLE IF NOT EXISTS CREDENTIALS
       (USERNAME VARCHAR(50) PRIMARY KEY NOT NULL,
-	  PASSWORD VARCHAR(50) NOT NULL,
-	  ONLINE BOOLEAN NOT NULL);''')
+	  PASSWORD VARCHAR(50) NOT NULL);''')
+
+cur.execute('''CREATE TABLE IF NOT EXISTS ONLINE
+      (USERNAME VARCHAR(50) PRIMARY KEY NOT NULL,
+	  PORT TEXT NOT NULL);''')
+
+cur.execute('''CREATE TABLE IF NOT EXISTS INDVMSSGS
+      (SENDER VARCHAR(50) NOT NULL,
+	  RECV VARCHAR(50) NOT NULL,
+	  MESSAGE TEXT NOT NULL);''')
+
+cur.execute('''CREATE TABLE IF NOT EXISTS GRPMSSGS
+      (GRPNAME VARCHAR(50) NOT NULL,
+	  SENDER VARCHAR(50) NOT NULL,
+	  MESSAGE TEXT NOT NULL);''')
 
 dbconn.commit()
 
+
 def clientthread(conn, addr):
+    username = 0
+    # sends a message to the client whose user object is conn
+    success = False
+    while not success:
+        inp = conn.recv(512)
+        inp = inp.decode('utf-8')
+        if (inp == "quit"):
+            remove(conn)
+            return
 
-	# sends a message to the client whose user object is conn
-	conn.send(b"Welcome to this chatroom!")
+        inp = inp.split(":")
+        username = inp[1]
+        to_check = f"SELECT * FROM CREDENTIALS WHERE USERNAME = '{inp[1]}' "
+        cur.execute(to_check)
+        selected_entry = cur.fetchone()
 
-	while True:
-			try:
-				message = conn.recv(2048)
-				if message:
+        if selected_entry == None:
+            if (inp[0] == "1"):
+                conn.send(bytes("n", 'utf-8'))
+            else:
+                postgres_insert_query = f'''INSERT INTO CREDENTIALS (USERNAME, PASSWORD) VALUES ('{inp[1]}', '{inp[2]}')'''
+                cur.execute(postgres_insert_query)
+                dbconn.commit()
+                conn.send(bytes("y", 'utf-8'))
+                success = True
+                to_check = f"SELECT * FROM CREDENTIALS WHERE USERNAME = '{inp[1]}' "
+                cur.execute(to_check)
+        else:
+            if (inp[0] == "2"):
+                conn.send(bytes("n", 'utf-8'))
+            else:
+                if (inp[2] == selected_entry[0]):
+                    conn.send(bytes("y", 'utf-8'))
+                    success = True
+                else:
+                    conn.send(bytes("n", 'utf-8'))
 
-					"""prints the message and address of the
-					user who just sent the message on the server
-					terminal"""
-					print ("<" + addr[0] + "> " + message.decode('utf-8'))
+    username_conn[username] = conn
+    while True:
+        try:
+            message = conn.recv(1).decode('utf-8')
+            print(message)
+            if message == "d":
+                to_usr = conn.recv(512).decode('utf-8')
+                to_msg = conn.recv(512)
+                print(to_usr)
 
-					# Calls broadcast function to send message to all
-					message_to_send = "<" + addr[0] + "> " + message.decode('utf-8')
-					print('here')
-					broadcast(message_to_send, conn)
-					print('m out')
+                find_usr = f"SELECT * FROM CREDENTIALS WHERE USERNAME = '{to_usr}' "
+                cur.execute(find_usr)
+                entry = cur.fetchone()
+                if entry == None:
+                    # Send client that user does not exist
+                    a = "user does not exist"
+                    conn.sendall(a.encode('utf-8'))
+                    pass
+                else:
+                    if to_usr in username_conn.keys():
+                        # Recieving user is active
+                        username_conn[to_usr].send(to_msg)
+                    else:
+                        postgres_insert_query = f'''INSERT INTO INDVMSSGS (SENDER, RECV, MESSAGE) VALUES ('{username}', '{to_usr}', '{to_msg.decode('utf-8')}')'''
+                        cur.execute(postgres_insert_query)
+                        dbconn.commit()
+            elif message == "g":
+                pass
+            elif message == "b":
+                pass
+            elif message == "l":
+                pass
+            if message:
 
-				else:
-					"""message may have no content if the connection
-					is broken, in this case we remove the connection"""
-					remove(conn)
+                """prints the message and address of the
+                user who just sent the message on the server
+                terminal"""
+                print("<" + addr[0] + "> " + message.decode('utf-8'))
 
-			except:
-				continue
+                # Calls broadcast function to send message to all
+                message_to_send = "<" + addr[0] + \
+                    "> " + message.decode('utf-8')
+                broadcast(message_to_send, conn)
+
+            else:
+                """message may have no content if the connection
+                is broken, in this case we remove the connection"""
+                remove(conn)
+
+        except:
+            continue
+
 
 """Using the below function, we broadcast the message to all
 clients who's object is not the same as the one sending
 the message """
-def broadcast(message, connection):
-	print('entered server 123')
-	for clients in list_of_clients:
-		if clients!=connection:
-			print('entered server')
-			try:
-				clients.send(bytes(message, 'utf-8'))
-			except:
-				clients.close()
 
-				# if the link is broken, we remove the client
-				remove(clients)
+
+def broadcast(message, connection):
+    for clients in list_of_clients:
+        if clients != connection:
+            try:
+                clients.send(bytes(message, 'utf-8'))
+            except:
+                clients.close()
+
+                # if the link is broken, we remove the client
+                remove(clients)
+
 
 """The following function simply removes the object
 from the list that was created at the beginning of
 the program"""
+
+
 def remove(connection):
-	if connection in list_of_clients:
-		list_of_clients.remove(connection)
+    if connection in list_of_clients:
+        list_of_clients.remove(connection)
+
 
 while True:
 
-	"""Accepts a connection request and stores two parameters,
-	conn which is a socket object for that user, and addr
-	which contains the IP address of the client that just
-	connected"""
-	conn, addr = server.accept()
+    """Accepts a connection request and stores two parameters,
+    conn which is a socket object for that user, and addr
+    which contains the IP address of the client that just
+    connected"""
+    conn, addr = server.accept()
 
-	"""Maintains a list of clients for ease of broadcasting
+    """Maintains a list of clients for ease of broadcasting
 	a message to all available people in the chatroom"""
-	list_of_clients.append(conn)
+    list_of_clients.append(conn)
 
-	# prints the address of the user that just connected
-	print (addr[0] + " connected")
+    # prints the address of the user that just connected
+    print(addr[0] + " connected")
 
-	# creates and individual thread for every user
-	# that connects
-	start_new_thread(clientthread,(conn,addr))	
+    # creates and individual thread for every user
+    # that connects
+    start_new_thread(clientthread, (conn, addr))
 
 dbconn.close()
 conn.close()
 server.close()
-
